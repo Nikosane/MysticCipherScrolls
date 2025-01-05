49637ea0eae9d066eb0158e22417bd6a1a757d4b
Nikosane <linux.nikosane@gmail.com>
2025-01-05 22:19:07 +0530

    scrolls and locations

---
+++ game/constants.py
@@ -0,0 +1,28 @@
+LOCATIONS = [
+    "The Forgotten Temple",
+    "Whispering Woods",
+    "The Shimmering Lake",
+    "The Tower of Shadows",
+    "The Mystic Caverns"
+]
+
+SCROLLS = [
+    {
+        "name": "Scroll of Knowledge",
+        "content": "Knowledge is the key to enlightenment.",
+        "encrypted": "Vjopqjl uo vjg mgdt vq jqpfogvjkogpv.",
+        "key": "shift_2"
+    },
+    {
+        "name": "Scroll of Wisdom",
+        "content": "Wisdom begins with curiosity.",
+        "encrypted": "Ykxusq cgnnq vjgp ectwkuv{.",
+        "key": "shift_4"
+    },
+    {
+        "name": "Scroll of Power",
+        "content": "True power lies in understanding.",
+        "encrypted": "Uhuh wpurk skgk ku xdfgukdwpv.",
+        "key": "vigenere_code"
+    }
+]
